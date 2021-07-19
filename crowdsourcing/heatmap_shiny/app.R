c2b512a630f0753e1a24c9f205ad7ae4309a8024
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2021-07-19 09:41:42 +0200

    started working on market integration paper idea (off selling to the market paper)

--- crowdsourcing/heatmap_shiny/app.R
+++ crowdsourcing/heatmap_shiny/app.R
@@ -81,7 +81,7 @@ tabPanel("Prices", fluid = TRUE,
       sliderInput("period_p",h4("Select period or push play button:"),
                   min = as.Date("2019-05-01"),max =as.Date("2019-07-01"),value=as.Date("2019-07-01"),step = 31, timeFormat="%b",animate =
                    animationOptions(interval = 5000, loop = TRUE)),
-                   radioButtons("crop_select_p","Crop", c("maize" = "maize", "soy bean" = "soy_bean"), inline=T),
+                   radioButtons("crop_select_p","Crop", c("maize" = "maize", "soybean" = "soy_bean"), inline=T),
                    radioButtons("year_select_p","Use data from", c("2020" = "2020", "2021" = "2021","2020 + 2021" = "both"), inline=T)
                    
     ),
@@ -99,7 +99,7 @@ tabPanel("Volumes", fluid = TRUE,
      sliderInput("period_q",h4("Select period or push play button:"),
                    min = as.Date("2019-05-01"),max =as.Date("2019-07-01"),value=as.Date("2019-07-01"),step = 31, timeFormat="%b",animate =
                    animationOptions(interval = 2000, loop = TRUE)),
-                   radioButtons("crop_select_q","Crop", c("maize" = "maize", "soy bean" = "soy_bean"), inline=T),
+                   radioButtons("crop_select_q","Crop", c("maize" = "maize", "soybean" = "soy_bean"), inline=T),
                   radioButtons("year_select_q","Use data from", c("2020" = "2020", "2021" = "2021","2020 + 2021" =  "both"), inline=T)
     ),
 
@@ -144,9 +144,9 @@ server <- function(input, output) {
 
   get_data_p <- reactive({
   if (input$year_select_p  == "both") {
-    aggregate(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4)  & data$crop == input$crop_select_p)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4)  & data$crop == input$crop_select_p)  ,]$TA_CODE), mean, na.rm=T)
+    aggregate(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4)  & data$crop == input$crop_select_p)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4)  & data$crop == input$crop_select_p)  ,]$TA_NAME), mean, na.rm=T)
     } else {
-     aggregate(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4) & data$year == input$year_select_p & data$crop == input$crop_select_p)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4) & data$year == input$year_select_p & data$crop == input$crop_select_p)  ,]$TA_CODE), mean, na.rm=T)  
+     aggregate(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4) & data$year == input$year_select_p & data$crop == input$crop_select_p)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_p, tz="GMT")) -4) & data$year == input$year_select_p & data$crop == input$crop_select_p)  ,]$TA_NAME), mean, na.rm=T)  
     }
     
   })
@@ -154,9 +154,9 @@ server <- function(input, output) {
   get_data_q <- reactive({
 
      if (input$year_select_q  == "both") {
-   aggregate( data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4)  & data$crop == input$crop_select_q)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4)  & data$crop == input$crop_select_q)  ,]$TA_CODE), mean, na.rm=T)
+   aggregate( data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4)  & data$crop == input$crop_select_q)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4)  & data$crop == input$crop_select_q)  ,]$TA_NAME), mean, na.rm=T)
     } else {
-      aggregate(data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4) & data$year == input$year_select_q & data$crop == input$crop_select_q)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4) & data$year == input$year_select_q & data$crop == input$crop_select_q)  ,]$TA_CODE), mean, na.rm=T)  
+      aggregate(data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4) & data$year == input$year_select_q & data$crop == input$crop_select_q)  ,][c("Price","lon_centroid","lat_centroid","kg","Quantity","TA_NAME")],list(data[which(data$pos == (month(as.POSIXct(input$period_q, tz="GMT")) -4) & data$year == input$year_select_q & data$crop == input$crop_select_q)  ,]$TA_NAME), mean, na.rm=T)  
     }
   })
 
@@ -172,7 +172,7 @@ clearControls() %>%
                   fillColor = ~colorNumeric(c("#008000","#FF0000"), domain =  data$Price , reverse = TRUE)(Price), 
                   fillOpacity = 0.9,
                   color = 'black',
-                  weight = 1.5, label = ~as.character(TA_NAME), popup = ~as.character(Price))  %>%
+                  weight = 1.5, label = ~as.character(Group.1), popup = ~as.character(round(Price)))  %>%
       addLegend(pal = colorNumeric(c("#008000","#FF0000"), domain =  data$Price , reverse = TRUE), values = data$Price, opacity = 0.7, title = NULL,
                 position = "topright") 
   })
@@ -181,7 +181,7 @@ clearControls() %>%
     data <- get_data_q()
      leafletProxy('volumes', data = data) %>%
   clearGroup('dots')     %>% clearControls() %>%
-          addCircleMarkers(group='dots',data=data, lng=~lon_centroid, lat=~lat_centroid,radius=~Quantity,  stroke = FALSE,color="red", label = ~as.character(TA_NAME), popup = ~as.character(kg),fillOpacity=.5)  %>% addLegendCustom(colors = c("red", "red", "red","red"), labels = c("400 kg", "800 kg", "1200 kg","1600 kg"), sizes = c(10, 20,30, 40))
+          addCircleMarkers(group='dots',data=data, lng=~lon_centroid, lat=~lat_centroid,radius=~Quantity,  stroke = FALSE,color="red", label = ~as.character(Group.1), popup = ~as.character(round(kg)),fillOpacity=.5)  %>% addLegendCustom(colors = c("red", "red", "red","red"), labels = c("400 kg", "800 kg", "1200 kg","1600 kg"), sizes = c(10, 20,30, 40))
   })
   
 

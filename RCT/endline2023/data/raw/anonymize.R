cf715607cc757172014feef26c0fece52a56383d
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2023-05-03 11:55:33 +0200

    about 90 percent done - started analysis for impact at endline

--- RCT/endline2023/data/raw/anonymize.R
+++ RCT/endline2023/data/raw/anonymize.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library(leaflet)
 library(reshape2)
 library(htmlwidgets)
@@ -17,6 +18,28 @@ read.csv(paste(path,"baseline/data/raw/latest_form2.csv", sep="/"))[c("farmer_ID
 ## zero. The second change is more fundamental when we changed all measures for gnuts from kgs to debe
 ## this needs to be coded - we can use the variable X_version which is a unique ID for the XLS_form that was used
 table(dta$X_version,dta$today)
+
+set <- c("check.maize.G_exp1", "check.maize.G_exp2","check.maize.G_exp3","check.maize.G_exp4")
+dta[set] <- lapply(dta[set],  function(x) as.numeric(as.character(x)))
+
+dta[set] <- lapply(dta[set],  function(x) ifelse(x==999,NA,x))
+
+dta$check.maize.G_exp1[dta$X_version %in% c("202304121045","202304170715")] <-  dta$check.maize.G_exp1[dta$X_version %in% c("202304121045","202304170715")] *7.4
+dta$check.maize.G_exp2[dta$X_version %in% c("202304121045","202304170715")] <-  dta$check.maize.G_exp2[dta$X_version %in% c("202304121045","202304170715")] *7.4
+dta$check.maize.G_exp3[dta$X_version %in% c("202304121045","202304170715")] <-  dta$check.maize.G_exp3[dta$X_version %in% c("202304121045","202304170715")] *7.4
+dta$check.maize.G_exp4[dta$X_version %in% c("202304121045","202304170715")] <-  dta$check.maize.G_exp4[dta$X_version %in% c("202304121045","202304170715")] *7.4
+
+### issues with missing farmer IDs - save those that are controls in second part
+
+dta$farmer_ID[dta$X_uuid =="82ad25af-1970-4a09-8934-4030aef73c1b"] <- "F_1352"
+dta$farmer_ID[dta$X_uuid =="34e4013e-66a7-43f6-b041-a965247a1b05"] <- "F_1340"
+dta$farmer_ID[dta$X_uuid =="a7f12052-8a2e-4ab5-8de3-41d598758f9f"] <- "F_1349"
+dta$farmer_ID[dta$X_uuid =="3162a5d9-58ca-4eb4-86a2-b0262f926229"] <- "F_1284"
+dta$farmer_ID[dta$X_uuid =="bfa56209-bea5-4971-b630-f9d465ed8048"] <- "F_1413"
+dta$farmer_ID[dta$X_uuid =="a4d4f16d-8ba7-4596-abb1-c653ddbe9429"] <- "F_1274"
+dta$farmer_ID[dta$X_uuid =="d85f3ee0-993b-49cc-8070-24cc3315dcd7"] <- "F_1356"
+dta$farmer_ID[dta$X_uuid =="2a408d7c-a4aa-4242-96a6-a82fa39f583d"] <- "F_1343"
+dta$farmer_ID[dta$X_uuid =="51117b46-0ec2-4f14-b987-dc365f86f7fc"] <- "F_1399"
 
 done <- names(table(dta$farmer_ID))
 #progress

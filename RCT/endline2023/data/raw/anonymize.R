7d9fb68b5876143b3050cc7a40429b2e7063870d
Bjorn Van Campenhout <bjorn.vancampenhout@gmail.com>
2025-04-25 18:43:07 +0200

    removed duplicates from endline_2023

--- RCT/endline2023/data/raw/anonymize.R
+++ RCT/endline2023/data/raw/anonymize.R
@@ -10,8 +10,8 @@ dta <- read.csv("latest.csv")
 path <- strsplit(path,"endline2023/data/raw")[[1]]
 
 dta_base <-
-rbind(read.csv(paste(path,"baseline/data/raw/latest_form1.csv", sep="/"))[c("farmer_ID","check.check2.maize._gps_longitude","check.check2.maize._gps_latitude")],
-read.csv(paste(path,"baseline/data/raw/latest_form2.csv", sep="/"))[c("farmer_ID","check.check2.maize._gps_longitude","check.check2.maize._gps_latitude")])
+rbind(read.csv(paste(path,"baseline/data/raw/latest_form1.csv", sep="/"))[c("farmer_ID","q3","check.check2.maize._gps_longitude","check.check2.maize._gps_latitude")],
+read.csv(paste(path,"baseline/data/raw/latest_form2.csv", sep="/"))[c("farmer_ID","q3","check.check2.maize._gps_longitude","check.check2.maize._gps_latitude")])
 
 
 ## three versions of the form were used. In the first change, this was just a change to the limits to allow enumerators to enter
@@ -53,6 +53,93 @@ pal <- colorFactor(c("red", "green"),dta_base$done)
 
 m <- leaflet() %>% setView(lat = -13.713, lng = 33.85, zoom=9)  %>%  addTiles(group="OSM") %>% addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",  group="Google", attribution = 'Google')  %>% addProviderTiles(providers$OpenTopoMap, group="Topography") %>% addCircleMarkers(data=dta_base, lng=~as.numeric(check.check2.maize._gps_longitude), lat=~as.numeric(check.check2.maize._gps_latitude),radius= 2,  color=~pal(done), popup = ~as.character(farmer_ID), group = "X_uuid")   %>%  addLayersControl(baseGroups=c('OSM','Google','Topography')) 
 saveWidget(m, file="malawi_progress.html") 
+
+#duplicates
+dta$farmer_ID[duplicated(dta$farmer_ID)]
+
+dupper <- "F_3425"
+#get name of village
+dupper_vil <- dta$q3[dta$farmer_ID == dupper][1] 
+#what farmers from baseline can not be matched with a farmer from enldine 
+unmatched <- setdiff(dta_base$farmer_ID[dta_base$q3 == dupper_vil],dta$farmer_ID[dta$q3 == dupper_vil])
+#so one of the unmatched farmers should be linked to one of the duplicates
+
+
+#let us put this on a map - in green the unmateched baselines and the duplicate
+
+dta_base <- dta_base[dta_base$farmer_ID %in% c(dupper,unmatched),c("farmer_ID", "q3", "check.check2.maize._gps_longitude", "check.check2.maize._gps_latitude")]
+names(dta_base) <- c("farmer_ID","village","longitude","latitude")
+dta_base$X_uuid <- NA
+dta_base$round <- "baseline"
+
+dup <- dta[dta$farmer_ID==dupper,c("farmer_ID","q3","check.maize._GPS_longitude","check.maize._GPS_latitude","X_uuid")]
+names(dup) <- c("farmer_ID","village","longitude","latitude","X_uuid")
+dup$round <- "endline"
+
+
+
+dup <- rbind(dup,dta_base)
+pal <- colorFactor(c("red", "green"),dup$round)
+m <- leaflet() %>% setView(lat = -13.713, lng = 33.85, zoom=9)  %>%  addTiles(group="OSM") %>% addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",  group="Google", attribution = 'Google')  %>% addProviderTiles(providers$OpenTopoMap, group="Topography") %>% addCircleMarkers(data=dup, lng=~as.numeric(longitude), lat=~as.numeric(latitude),radius= 2,  color=~pal(round), popup = ~as.character(farmer_ID), group = "X_uuid")   %>%  addLayersControl(baseGroups=c('OSM','Google','Topography')) 
+
+mX <- leaflet() %>% setView(lat = -13.713, lng = 33.85, zoom=9)  %>%  addTiles(group="OSM") %>% addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",  group="Google", attribution = 'Google')  %>% addProviderTiles(providers$OpenTopoMap, group="Topography") %>% addCircleMarkers(data=dup, lng=~as.numeric(longitude), lat=~as.numeric(latitude),radius= 2,  color=~pal(round), popup = ~as.character(X_uuid), group = "X_uuid")   %>%  addLayersControl(baseGroups=c('OSM','Google','Topography')) 
+
+dta <- dta[dta$X_uuid !="cac3c860-909a-440f-b23f-f6aa2b91fdb5",] #fix F_477
+dta <- dta[dta$X_uuid !="cde953b8-4f25-4e92-97c2-8565a633280d",] #fix F_1195
+dta <- dta[dta$X_uuid !="ac68b8d6-82c6-4054-83b4-74c0097190f6",] #fix F_1356
+dta <- dta[dta$X_uuid !="9bf2acd1-f7d4-4ad6-8f11-3718cfa0ae73",] #fix F_2173
+dta <- dta[dta$X_uuid !="b070c736-8a05-48c1-84ca-0fe64fe8e28d",] #fix F_2945
+dta <- dta[dta$farmer_ID !="F_2953",]
+dta <- dta[dta$farmer_ID !="F_2264",]
+dta <- dta[dta$farmer_ID !="F_2266",]
+dta <- dta[dta$X_uuid !="4bca98bd-1d66-4e7d-a2a2-44a7d0ca5904",] #fix F_2342
+dta <- dta[dta$X_uuid !="e132965b-c3db-419a-bbba-49e586e0220d",] #fix F_2347
+dta <- dta[dta$farmer_ID !="F_2521",]
+dta <- dta[dta$farmer_ID !="F_2531",]
+dta <- dta[dta$farmer_ID !="F_2374",]
+dta <- dta[dta$X_uuid !="1661d0f8-8184-451e-8b3a-ffcdbe328a21",] #fix F_2368
+dta <- dta[dta$farmer_ID !="F_3016",]
+dta <- dta[dta$farmer_ID !="F_2385",]
+dta <- dta[dta$farmer_ID !="F_2524",]
+dta <- dta[dta$farmer_ID !="F_2534",]
+dta <- dta[dta$farmer_ID !="F_2494",]
+dta <- dta[dta$X_uuid !="450cc225-55ee-435a-92ac-d57bc9822261",]
+dta <- dta[dta$farmer_ID !="F_1072",]
+dta <- dta[dta$farmer_ID !="F_3540",]
+dta <- dta[dta$X_uuid !="c40aa25e-31db-4f1a-a987-8473f22b223c",] #fix F_1084
+dta <- dta[dta$farmer_ID !="F_518",]
+dta <- dta[dta$farmer_ID !="F_1765",]
+dta <- dta[dta$farmer_ID !="F_1284",]
+dta <- dta[dta$farmer_ID !="F_2297",]
+dta <- dta[dta$farmer_ID !="F_1274",]
+dta <- dta[dta$farmer_ID !="F_506",]
+dta <- dta[dta$farmer_ID !="F_512",]
+dta <- dta[dta$farmer_ID !="F_897",]
+dta <- dta[dta$farmer_ID !="F_135",]
+dta <- dta[dta$farmer_ID !="F_3425",]
+dta <- dta[dta$farmer_ID !="n/a",]
+dta$farmer_ID[duplicated(dta$farmer_ID)]
+
+#dupper <- "F_3425"
+#dup <- dta[dta$farmer_ID==dupper,c("farmer_ID","q3","check.maize._GPS_longitude","check.maize._GPS_latitude","X_uuid")]
+#names(dup) <- c("farmer_ID","village","longitude","latitude","X_uuid")
+#dup$round <- "endline"
+
+
+#dta_base <- dta_base[dta_base$q3 == dta$q3[dta$farmer_ID == dupper][1],c("farmer_ID", "q3", "check.check2.maize._gps_longitude", "check.check2.maize._gps_latitude")]
+#names(dta_base) <- c("farmer_ID","village","longitude","latitude")
+#dta_base$X_uuid <- NA
+#dta_base$round <- "baseline"
+
+
+#dup <- rbind(dup,dta_base)
+
+
+#pal <- colorFactor(c("red", "green"),dup$round)
+#m <- leaflet() %>% setView(lat = -13.713, lng = 33.85, zoom=9)  %>%  addTiles(group="OSM") %>% addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",  group="Google", attribution = 'Google')  %>% addProviderTiles(providers$OpenTopoMap, group="Topography") %>% addCircleMarkers(data=dup, lng=~as.numeric(longitude), lat=~as.numeric(latitude),radius= 2,  color=~pal(round), popup = ~as.character(farmer_ID), group = "X_uuid")   %>%  addLayersControl(baseGroups=c('OSM','Google','Topography')) 
+
+#mX <- leaflet() %>% setView(lat = -13.713, lng = 33.85, zoom=9)  %>%  addTiles(group="OSM") %>% addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",  group="Google", attribution = 'Google')  %>% addProviderTiles(providers$OpenTopoMap, group="Topography") %>% addCircleMarkers(data=dup, lng=~as.numeric(longitude), lat=~as.numeric(latitude),radius= 2,  color=~pal(round), popup = ~as.character(X_uuid), group = "X_uuid")   %>%  addLayersControl(baseGroups=c('OSM','Google','Topography')) 
+
 
 to_drop <- c(
   "start",                                        
